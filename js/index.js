2ded24b08ce61304667fb9df9d799b0fb248ffc8
rhanysz0277 <rhanysz0277@gmail.com>
2022-07-16 19:29:50 -0500

    made more button work on first card
    will probably have to change how this works in order for my idea to animate and display how I want it to

--- js/index.js
+++ js/index.js
@@ -1,3 +1,14 @@
 $(".more").on("click", function(){
-    $(".card").css("height","90%")
+    $(".card").css("height","90%");
+    $(".card2").css("display","none");
+    $(".card3").css("display","none");
+    $(".more").css("display","none");
+    $(".less").css("display","contents");
+})
+$(".less").on("click", function(){
+    $(".card").css("height","229px");
+    $(".card2").css("display","flex");
+    $(".card3").css("display","flex");
+    $(".more").css("display","contents");
+    $(".less").css("display","none");
 })
